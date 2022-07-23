b64f7dcebfc544ee919a84b78509bbe8569c29f7
Kenson Hui <kensonhui@v1040-wn-rt-a-104-55.campus-dynamic.uwaterloo.ca>
2022-07-23 17:39:25 -0400

    Duplicate zeros

---
+++ 1089-duplicate-zeros.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def duplicateZeros(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: None Do not return anything, modify arr in-place instead.
+        """
+        output = [-1 for i in range(0, len(arr))]
+        i = 0
+        j = 0
+        while j < len(arr):
+            if (arr[i] != 0):
+                output[j] = arr[i]
+            else:
+                output[j] = 0
+                if (j + 1 < len(arr)):
+                    output[j + 1] = 0
+                j += 1
+            i += 1
+            j += 1
+        i = 0
+        while (i < len(arr)):
+            arr[i] = output[i]
+            i += 1
+        return output
