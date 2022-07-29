0ba8f0aabc4a876fd0a6fd67c7ba8588b3292a2c
Kenson Hui <kensonhui@v1040-wn-rt-a-78-87.campus-dynamic.uwaterloo.ca>
2022-07-28 20:46:52 -0400

    Palindromic substrings

---
+++ 647-palindromic-substrings.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        not optimal in the general case
+        :type s: str
+        :rtype: int
+        """
+        palindrome_count = 0
+        backwards_s = s[::-1]
+        for i in range(1, len(s) + 1):
+            for j in range(0, len(s) - i + 1):
+                if s[j:j+i] == backwards_s[len(s) - j - i :len(s) - j]:
+                    palindrome_count += 1
+        return palindrome_count
